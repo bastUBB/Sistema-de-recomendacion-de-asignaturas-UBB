5329500fe10f704dfcb44efef23ebaeb809bd12c
BastiRC <bastian.rodriguez2101@alumnos.ubiobio.cl>
2025-05-26 17:04:20 -0400

    MOD archivos para la primera subida al servidor

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Cargar .env desde la carpeta actual
+dotenv.config({
+  path: path.resolve(__dirname, '.env')
+});
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
