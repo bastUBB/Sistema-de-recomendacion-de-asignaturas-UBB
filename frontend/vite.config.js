cadb511ed23eab11cafff1b41d58a07c6144aab7
BastiRC <bastian.rodriguez2101@alumnos.ubiobio.cl>
2025-05-27 14:26:20 -0400

    feat: Implement user management features including user registration, login, and user list with edit and delete functionalities
    - Added `useEditUser` hook for handling user updates.
    - Created `useGetUsers` hook to fetch and manage user data.
    - Updated `main.jsx` to set up routing with protected routes.
    - Developed user interface components for user management in `Users.jsx`.
    - Implemented user registration functionality in `Register.jsx`.
    - Added error handling and success alerts using SweetAlert.
    - Created a new `Error404` page for handling not found routes.
    - Styled components with new CSS files for better UI/UX.
    - Set up Axios service for API calls with authentication.
    - Added animations and responsive design for various components.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,17 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  preview: {port:1226, host:true},
+  preview: {port:443, host:true},
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@helpers': path.resolve(__dirname, './src/helpers')
+    }
+  }
 });
